a0c61a10c2930579a74b6719fd1d3850a0f82561
nagracks <nagracks@users.noreply.github.com>
2017-03-22 23:19:59 +0530

    Make directory if it doesn't exist with exit_ok=True

--- oamdl/__init__.py
+++ oamdl/__init__.py
@@ -15,7 +15,7 @@ Options:
         <NUM>       with this release number
         -r          with release numbers from <START> to <END>
         -a          in its entirety  (may take a while)
-        path        with the root directory OAM in this directory 
+        path        with the root directory OAM in this directory
         pathc       same as path, but create the directory if it does not exist
   -h, --help       Show help
       --version    Show version
@@ -52,14 +52,14 @@ def create():
             #Save the date
             day = re.match(r'^(\d+)-(\d+)-(\d+)', links[oam_num])
             comic_info['date'] = {}
-            
+
             comic_info['date']['year'] = int(day.group(1))
             comic_info['date']['month'] = int(day.group(2))
             comic_info['date']['day'] = int(day.group(3))
 
             #Save the link
             comic_info['link'] = links[oam_num]
-            
+
             OAM_DICT[oam_num] = comic_info
         with open(OAM_DICT_FILENAME, 'w') as f:
             json.dump(OAM_DICT, f)
@@ -82,17 +82,14 @@ def is_valid_release_num(OAM_DICT, num):
 #Download a single comic
 def download_one(OAM_DICT, comic):
     comic = str(comic)
-    
-    if not os.path.exists(ROOT_DIR):
-        os.makedirs(ROOT_DIR)
+
+    os.makedirs(ROOT_DIR, exist_ok=True)
 
     year_dir = os.path.join(ROOT_DIR, str(OAM_DICT[comic]['date']['year']))
-    if not os.path.exists(year_dir):
-        os.makedirs(year_dir)
+    os.makedirs(year_dir, exist_ok=True)
 
     month_dir = os.path.join(year_dir, str(OAM_DICT[comic]['date']['month']))
-    if not os.path.exists(month_dir):
-        os.makedirs(month_dir)
+    os.makedirs(month_dir, exist_ok=True)
     comic_path = os.path.join(month_dir, str(OAM_DICT[comic]['link']))
     if not os.path.exists(comic_path):
         with open(comic_path, 'wb') as c:
@@ -151,7 +148,7 @@ def download_release_num():
         num = int(arguments["<NUM>"])
     except ValueError:
         int_value_error_msg()
-        return 
+        return
 
     if OAM_DICT and is_valid_release_num(OAM_DICT, num):
         download_one(OAM_DICT, num)
@@ -162,17 +159,17 @@ def download_release_num_range():
     OAM_DICT = read_in_OAM_DICT()
     if OAM_DICT is None:
         return
-    
+
     try:
         start = int(arguments["<START>"])
         end = int(arguments["<END>"])
     except ValueError:
         int_value_error_msg()
-        return 
+        return
 
     if start > end:
         print("Start must be lower than N")
-        return 
+        return
 
     if is_valid_release_num(OAM_DICT, start) and is_valid_release_num(OAM_DICT, end):
         try:
